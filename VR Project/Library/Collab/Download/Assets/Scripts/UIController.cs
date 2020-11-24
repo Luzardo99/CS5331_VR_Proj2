6be9275c334a70cac5e7456c8a006e13bf737e9b
Luzardo99 <ruedaluzardo@gmail.com>
2020-11-23 23:32:49 -0600

    Unity Proj

---
+++ VR Project/Library/Collab/Download/Assets/Scripts/UIController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Action OnRoadPlacement, OnHousePlacement, OnSpecialPlacement, OnBankPlacement;
+    public Button placeRoadButton, placeHouseButton, placeSpecialButton, placeBankButton;
+
+    public Color outlineColor;
+    List<Button> buttonList;
+
+    private void Start()
+    {
+        buttonList = new List<Button> { placeHouseButton, placeRoadButton, placeSpecialButton, placeBankButton };
+
+        placeRoadButton.onClick.AddListener(() =>
+        {
+            ResetButtonColor();
+            ModifyOutline(placeRoadButton);
+            OnRoadPlacement?.Invoke();
+
+        });
+        placeHouseButton.onClick.AddListener(() =>
+        {
+            ResetButtonColor();
+            ModifyOutline(placeHouseButton);
+            OnHousePlacement?.Invoke();
+
+        });
+        placeSpecialButton.onClick.AddListener(() =>
+        {
+            ResetButtonColor();
+            ModifyOutline(placeSpecialButton);
+            OnSpecialPlacement?.Invoke();
+
+        });
+        placeBankButton.onClick.AddListener(() =>
+        {
+            ResetButtonColor();
+            ModifyOutline(placeBankButton);
+            OnBankPlacement?.Invoke();
+
+        });
+    }
+
+    private void ModifyOutline(Button button)
+    {
+        var outline = button.GetComponent<Outline>();
+        outline.effectColor = outlineColor;
+        outline.enabled = true;
+    }
+
+    private void ResetButtonColor()
+    {
+        foreach (Button button in buttonList)
+        {
+            button.GetComponent<Outline>().enabled = false;
+        }
+    }
+}
